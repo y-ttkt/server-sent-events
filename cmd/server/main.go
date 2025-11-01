b71cae77a3c215b2275448185b7e82f4643a846a
y-ttkt <ys.taka44@gmail.com>
2025-11-01 13:15:18 +0900

    feat:http server construction

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
 	mux := http.NewServeMux()
@@ -11,4 +18,48 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("OK"))
 	})
+
+	s := http.Server{
+		Addr:              fmt.Sprintf(":%s", getenv("PORT", "8080")),
+		Handler:           logMiddleware(corsMiddleware(mux)),
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	fmt.Printf("Listening on port %s\n", s.Addr)
+	if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("http server failed: %v", err)
+	}
+}
+
+func logMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("%s %s %s", r.Method, r.URL.Path, time.Since(start))
+	})
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodOptions {
+			allowOrigin := os.Getenv("ALLOW_ORIGIN")
+			if allowOrigin != "" {
+				w.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+				w.Header().Set("Access-Control-Allow-Credentials", "true")
+				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+				w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+			}
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func getenv(key, fallback string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+
+	return fallback
 }
